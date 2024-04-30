439e5e2c50cec6367f1963e743fc34c88590c960
Utsav Patel <158522755+utsavpatel562@users.noreply.github.com>
2024-04-29 21:00:07 -0400

    Update TotalAmount.js

--- src/components/TotalAmount.js
+++ src/components/TotalAmount.js
@@ -1,15 +1,21 @@
 // components/TotalAmount.js
-import React from 'react';
+import React from 'react'; 
+// Import the React library for creating components
 
+// Functional component definition for TotalAmount
 const TotalAmount = ({ total }) => {
-	return (
-		<div className="total">
-			<h3>
-				Total Amount:
-				${total.toFixed(2)}
-			</h3>
-		</div>
-	);
+  return (
+    <div className="total"> 
+      {/* Wrapper for the total amount display */}
+      
+      <h3> 
+        {/* Heading element to display the total amount */}
+        Total Amount: ${total.toFixed(2)} 
+        {/* Display the total, formatted to 2 decimal places */}
+      </h3>
+    </div>
+  );
 };
 
+// Export the TotalAmount component for use in other parts of the application
 export default TotalAmount;
